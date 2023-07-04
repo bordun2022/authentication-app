e8b299c1691b052f7acb3d5b8a265f301b2ae84d
Alyona <104214804+bordun2022@users.noreply.github.com>
2023-07-04 13:18:53 +0300

    added app-files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { RouterProvider, createHashRouter, } from 'react-router-dom';
+
+
+import ErrorPage from './pages/Error';
+import HomePage from './pages/Home';
+import RootLayout from './pages/Root';
+import AuthenticationPage, {
+  action as authAction,
+} from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './util/auth';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+
+  return (
+    <PageContent title="Welcome!" />
+  );
+}
+
+export default HomePage;
